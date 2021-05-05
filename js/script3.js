ebf1a8481e3162b17b9b46ec2b5b5af693ab5d41
Lauraavalentina <laura4mp@gmail.com>
2021-05-05 17:45:09 -0500

    Clase Mayo 5 condicionales y operadores lógicos

---
+++ js/script3.js
@@ -0,0 +1,21 @@
+function Asistencia() {
+    let asiste = document.getElementById("respuesta").value;
+  
+    if (asiste === "presente") {
+        alert("El estudiante está presente");
+      }
+
+      else {
+        alert("El estudiante no está presente");
+      }
+    } 
+
+    function mama(){
+        let respuesta = "pase";
+
+        if (respuesta === "pase"){
+            alert("Felicitarlo")
+        } else {
+            alert("Le pega")
+        }
+    }
